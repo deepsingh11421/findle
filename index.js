a2d14b152319530de0d0b8dcf6dadc8f3d182504
deepsingh11421 <deepsingh11421@gmail.com>
2020-02-03 16:00:35 +0530

    Setting up directories

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const port = 8000;
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
+app.get('/',function(req,res){
+    return res.render('home');
+});
+
+app.listen(port, function(err){
+    if(err){
+        console.log('Error in running the server');
+    }
+    console.log('Server running on port ',port);
+});
