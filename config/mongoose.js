4a8fb15de90c157a66b2484fc619de195f630be7
deepsingh11421 <deepsingh11421@gmail.com>
2020-02-09 11:07:53 +0530

    Setting up mongo-store to store session cookie

--- config/mongoose.js
+++ config/mongoose.js
@@ -8,3 +8,5 @@ db.on('error',console.error.bind(console,'Error conecting to DB'));
 db.once('open',function(){
     console.log('Successfully connected to DB');
 });
+
+module.exports = db;
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const cookieParser = require('cookie-parser');
 const sassMiddleware = require('node-sass-middleware');
+const MongoStore = require('connect-mongo')(session);
 const app = express();
 
 app.set('view engine','ejs');
@@ -18,7 +19,7 @@ app.use(sassMiddleware({
     src: './assets/scss',
     dest: './assets/css',
     debug: true,
-    outputStyle: 'extended',
+    outputStyle: 'expanded',
     prefix: '/css'
 }));
 app.set('layout extractStyles',true);
@@ -33,7 +34,13 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (100*60*100) //millisec
-    }
+    },
+    store: new MongoStore({
+        mongooseConnection: db,
+        autoRemove: 'disabled'
+    }, function(err){
+        console.log(err || 'connect mongo-db setup ok');
+    })
 }));
 
 app.use(passport.initialize());
